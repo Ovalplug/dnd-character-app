4925cb127dfd950c712071a9dcad4c7daae68955
Ovalplug <50775281+Ovalplug@users.noreply.github.com>
2026-02-23 11:58:41 +0000

    got this to a happy place for now

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <router-view v-if="loaded" />
+  <nav-bar />
+  <div class="app-container" v-if="loaded">
+    <router-view />
+  </div>
   <div v-else>Loading...</div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { useCharacterStore } from './stores/characterStore'
+import NavBar from './views/NavBar.vue'
 
 const store = useCharacterStore()
 const loaded = ref(false)
@@ -15,3 +19,9 @@ onMounted(async () => {
   loaded.value = true
 })
 </script>
+
+<style>
+.app-container {
+    margin-left: 64px;
+}
+</style>
---
+++ src/stores/dataStore.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+import charOfficial from './char-official.json'
+import charUA from './char-ua.json'
+
+const datasets = [charOfficial, charUA] as any[]
+export const allData = datasets.flat()
+
+export const useDataStore = defineStore('data', {
+  state: () => ({
+    loaded: false
+  }),
+
+  actions: {
+  }
+})
