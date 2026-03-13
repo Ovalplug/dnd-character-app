d4e3e1a5c1fe48319f7825f8dddc9904c5d89ddc
Ovalplug <50775281+Ovalplug@users.noreply.github.com>
2026-03-13 11:07:53 +0000

    better resources page

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,37 +1,33 @@
 <template>
   <div>
-    <h1>Resources</h1>
+    <div class="resources-header">
+      <h1>Resources</h1>
+      <div v-if="dataSelected" class="dropdown-group">
+        <select @change="onDropdownChange($event)" :value="selectedType">
+          <option value="races">Races</option>
+          <option value="backgrounds">Backgrounds</option>
+          <option value="classes">Classes</option>
+          <option value="feats">Feats</option>
+          <option value="eInvocations">Eldritch Invocations</option>
+          <option value="aInfusions">Artificer Infusions</option>
+          <option value="spells">Spells</option>
+        </select>
+      </div>
+    </div>
 
-    <div v-if="!dataStore.loaded">
+    <div v-if="!dataStore.loaded" class="content-under-header">
       <Loading message="Loading resources..." :size="96" variant="bold"></Loading>
     </div>
 
-    <div v-else>
-      <div v-if="debug">
-        <button @click="showData('all')">All data</button>
-        <button @click="showData('backgroundFluff')">Background fluff</button>
-        <button @click="showData('subclasses')">Subclasses</button>
-        <button @click="showData('raceFluff')">Race fluff</button>
-        <button @click="showNoData">Hide all</button>
-      </div>
-      <button @click="showData('races')">Races</button>
-      <button @click="showData('backgrounds')">Backgrounds</button>
-      <button @click="showData('classes')">Classes</button>
-      <button @click="showData('feats')">Feats</button>
-      <button @click="showData('eInvocations')">Eldritch Invocations</button>
-      <button @click="showData('aInfusions')">Artificer Infusions</button>
-      <button @click="showData('spells')">Spells</button>
-      <div v-if="debug">
-        <pre v-if="show_rawDatasets">{{ dataStore.rawDatasets }}</pre>
-        <pre v-if="show_feats">{{ dataStore.feats }}</pre>
-        <pre v-if="show_races">{{ dataStore.races }}</pre>
-        <pre v-if="show_backgrounds">{{ dataStore.backgrounds }}</pre>
-        <pre v-if="show_backgroundFluff">{{ dataStore.backgroundFluff }}</pre>
-        <pre v-if="show_raceFluff">{{ dataStore.raceFluff }}</pre>
-        <pre v-if="show_classes">{{ dataStore.classes }}</pre>
-        <pre v-if="show_eInvocations">{{ dataStore.eInvocations }}</pre>
-        <pre v-if="show_aInfusions">{{ dataStore.aInfusions }}</pre>
-        <pre v-if="show_subclasses">{{ dataStore.subclasses }}</pre>
+    <div v-else class="content-under-header">
+      <div v-if="!dataSelected" class="button-list">
+        <button @click="showData('races')">Races</button>
+        <button @click="showData('backgrounds')">Backgrounds</button>
+        <button @click="showData('classes')">Classes</button>
+        <button @click="showData('feats')">Feats</button>
+        <button @click="showData('eInvocations')">Eldritch Invocations</button>
+        <button @click="showData('aInfusions')">Artificer Infusions</button>
+        <button @click="showData('spells')">Spells</button>
       </div>
       <AllFeats v-if="show_feats" :feats="dataStore.feats" />
       <AllRaces v-if="show_races" :races="dataStore.races" :raceFluff="dataStore.raceFluff" />
@@ -53,7 +49,7 @@
 </template>
 
 <script lang="ts" setup>
-  import { onMounted, ref } from 'vue';
+  import { onMounted, ref, computed } from 'vue';
   import { useDataStore } from '../stores/dataStore';
   import Loading from '../components/resources/Loading.vue';
   import { useDebug } from '../composables/useDebug';
@@ -79,6 +75,30 @@
   const show_aInfusions = ref(false);
   const show_subclasses = ref(false);
   const show_spells = ref(false);
+
+  // Track which data type is currently selected
+  const selectedType = ref('');
+
+  // Computed: is any data selected?
+  const dataSelected = computed(() => {
+    return (
+      show_feats.value ||
+      show_races.value ||
+      show_backgrounds.value ||
+      show_backgroundFluff.value ||
+      show_raceFluff.value ||
+      show_classes.value ||
+      show_eInvocations.value ||
+      show_aInfusions.value ||
+      show_subclasses.value ||
+      show_spells.value
+    );
+  });
+
+  function onDropdownChange(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    showData(value);
+  }
 
   onMounted(async () => {
     await initDebug();
@@ -108,54 +128,110 @@
   }
 
   function showData(dataType: string) {
+    showNoData();
     switch (dataType) {
       case 'all':
-        showNoData();
         show_rawDatasets.value = true;
+        selectedType.value = '';
         break;
       case 'feats':
-        showNoData();
         show_feats.value = true;
+        selectedType.value = 'feats';
         break;
       case 'races':
-        showNoData();
         show_races.value = true;
+        selectedType.value = 'races';
         break;
       case 'backgrounds':
-        showNoData();
         show_backgrounds.value = true;
+        selectedType.value = 'backgrounds';
         break;
       case 'backgroundFluff':
-        showNoData();
         show_backgroundFluff.value = true;
+        selectedType.value = 'backgroundFluff';
         break;
       case 'raceFluff':
-        showNoData();
         show_raceFluff.value = true;
+        selectedType.value = 'raceFluff';
         break;
       case 'classes':
-        showNoData();
         show_classes.value = true;
+        selectedType.value = 'classes';
         break;
       case 'eInvocations':
-        showNoData();
         show_eInvocations.value = true;
+        selectedType.value = 'eInvocations';
         break;
       case 'aInfusions':
-        showNoData();
         show_aInfusions.value = true;
+        selectedType.value = 'aInfusions';
         break;
       case 'subclasses':
-        showNoData();
         show_subclasses.value = true;
+        selectedType.value = 'subclasses';
         break;
       case 'spells':
-        showNoData();
         show_spells.value = true;
+        selectedType.value = 'spells';
         break;
       default:
-        showNoData();
+        selectedType.value = '';
         break;
     }
   }
-</script>
+ </script>
+
+<style scoped>
+.resources-header {
+  position: fixed;
+  top: 0;
+  left: 40px;
+  width: calc(100% - 40px);
+  background: #e0e0e0;
+  z-index: 49;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 24px;
+  box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+}
+
+.resources-header h1 {
+  margin: 0;
+  font-size: 2rem;
+}
+
+.dropdown-group {
+  margin-left: 24px;
+}
+
+.dropdown-group select {
+  font-size: 1rem;
+  padding: 8px 12px;
+}
+
+.content-under-header {
+  margin-top: 80px;
+}
+
+.button-list {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  max-width: 320px;
+  margin: 32px auto;
+}
+
+.button-list button {
+  font-size: 1.1rem;
+  padding: 12px 18px;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  background: #f7f7f7;
+  cursor: pointer;
+  transition: background 0.2s;
+}
+.button-list button:hover {
+  background: #e0e0e0;
+}
+</style>
