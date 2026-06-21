e0ab6412a1b1441fe55a97337a10a46657c65eea
Ovalplug <50775281+Ovalplug@users.noreply.github.com>
2026-06-21 17:33:05 +0100

    loading fixes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div v-if="!dataStore.loaded" class="content-under-header">
+      <Loading message="Loading resources..." :size="96" variant="bold"></Loading>
+    </div>
+  <div v-else>
     <h1>Welcome to D&D Character Manager</h1>
     <p>
       This is your home screen. Use the navigation to manage your characters, view resources, and
@@ -9,7 +12,22 @@
 </template>
 
 <script setup lang="ts">
-  // Home screen does not require logic currently
+import Loading from '../components/resources/Loading.vue';
+
+import { useDataStore } from '../stores/dataStore';
+import { onMounted } from 'vue';
+const dataStore = useDataStore();
+onMounted(async () => {
+    if (!dataStore.loaded) {
+      try {
+        await dataStore.init();
+      } catch (err) {
+        // keep this simple; devs can improve error handling/UI later
+        // eslint-disable-next-line no-console
+        console.error('Failed to load data store', err);
+      }
+    }
+  });
 </script>
 
 <style scoped>
